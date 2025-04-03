254557034d33484193dbc6611d55e7e7b6a0098a
Edward Kemper <edwardkemper@gmail.com>
2025-04-03 08:29:42 -0600

    foofoo

--- convert_to_pdf/convert.py
+++ convert_to_pdf/convert.py
@@ -1,13 +1,13 @@
 import pdfkit
 
-output_file = "Edward Kemper Cover Letter - Truelogic.pdf"
+output_file = "Edward Kemper resume - OpenLoop TPM.pdf"
 
 def html_to_pdf(input_html, output_file):
     pdfkit.from_file(input_html, output_file)
 
 if __name__ == "__main__":
         
-    input_file = "../get_suggestions/temp_coverletter.txt"
+    input_file = "resume-proto-OpenLoop-TPM.html"
     
 
     # Optional config if wkhtmltopdf isn't in your PATH:
